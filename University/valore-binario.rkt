593daa74fd6875bd0fe0855a671d9296bf4e3c56
Leonardo Rinaldi <rinaldileonardo99@gmail.com>
2020-10-20 14:12:13 +0200

    Update valore-binario.rkt
    fix typo

--- University/valore-binario.rkt
+++ University/valore-binario.rkt
@@ -1,6 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname valore-binario) (read-case-sensitive #t) (teachpacks ((lib "drawings.rkt" "installed-teachpacks"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "drawings.rkt" "installed-teachpacks")) #f)))
+;Conversione di un numero intero in binario
+
 (define bit    ; val: intero
   (lambda (b)  ; b: stringa
     (if (string=? b "0")
