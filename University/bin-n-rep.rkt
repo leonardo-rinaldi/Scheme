df3149aa18b939da18deb792436f91d8704b2199
Leonardo Rinaldi <rinaldileonardo99@gmail.com>
2020-10-20 14:13:35 +0200

    Update bin-n-rep.rkt

--- University/bin-n-rep.rkt
+++ University/bin-n-rep.rkt
@@ -1,6 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname bin-n-rep) (read-case-sensitive #t) (teachpacks ((lib "drawings.rkt" "installed-teachpacks"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "drawings.rkt" "installed-teachpacks")) #f)))
+; Conversione di un numero n in base 10 ad una base b
+
 (define bin-n-rep ;val: stringa (n in base b)
   (lambda (n b)   ; n: intero b: intero
     (let
