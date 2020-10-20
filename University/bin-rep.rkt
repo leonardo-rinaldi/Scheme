55ab4b2fc126b6c002e7dcc76fb38d986398e349
Leonardo Rinaldi <rinaldileonardo99@gmail.com>
2020-10-20 14:14:31 +0200

    Update bin-rep.rkt

--- University/bin-rep.rkt
+++ University/bin-rep.rkt
@@ -1,6 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname bin-rep) (read-case-sensitive #t) (teachpacks ((lib "drawings.rkt" "installed-teachpacks"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "drawings.rkt" "installed-teachpacks")) #f)))
+;Conversione di un numero n in base 10 alla base 2
+
 (define bin-rep  ; val: stringa (n in base 2)
   (lambda (n)    ; n: intero
     (let
