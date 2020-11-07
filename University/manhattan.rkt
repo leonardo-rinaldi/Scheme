4c19c82d2e7003b901516f369613a940bfa723f9
Leonardo Rinaldi <rinaldileonardo99@gmail.com>
2020-11-07 11:34:17 +0100

    Add files via upload

---
+++ University/manhattan.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname manhattan) (read-case-sensitive #t) (teachpacks ((lib "drawings.rkt" "installed-teachpacks"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "drawings.rkt" "installed-teachpacks")) #f)))
+; Esercizio sui percorsi di Manhattan
+
+(define manh     ;val: intero
+  (lambda (i j)  ; i, j: interi non negativi
+    (cond ((= i 0) 1)
+          ((= j 0) 1)
+          (else
+           (+ (manh (- i 1) j)
+              (manh i (- j 1)))
+           ))
+    ))
+
+
+
